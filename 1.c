d6e8373b16907fefb9d099e33c5aadcc14ce0aed
pandora1881 <igor.migalushhhh@gmail.com>
2023-05-31 09:15:38 +0300

    lab8

--- 1.c
+++ 1.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int main()
-{
+int main() {
     int n = 10;
     int a[] = {-4, 2, 2, -3, 4, 2, -4, 1, -2, 0};
 
-    int maxMagnitude = 0;
-    int indices[n];
-    int count = 0;
+    int max_absolute_value = abs(*a);  // Початкове найбільше значення
+    int* max_indices[10];  // Масив вказівників на найбільші числа
+    int count = 0;  // Лічильник знайдених найбільших чисел
 
-    // Знаходимо максимальний модуль числа
-    for (int i = 0; i < n; i++)
-    {
-        int magnitude = abs(a[i]);
-        if (magnitude > maxMagnitude)
-        {
-            maxMagnitude = magnitude;
+    // Перебір елементів масиву і пошук найбільших чисел
+    for (int i = 0; i < n; i++) {
+        int current_value = abs(*(a + i));  // Абсолютне значення поточного числа
+
+        if (current_value > max_absolute_value) {
+            max_absolute_value = current_value;
+            count = 0;  // Знайдено нове максимальне число, скидаємо лічильник
+        }
+
+        if (current_value == max_absolute_value) {
+            max_indices[count] = &a[i];  // Зберігаємо адресу числа у масиві
+            count++;  // Збільшуємо лічильник
         }
     }
 
-    // Знаходимо індекси чисел з максимальним модулем та друкуємо їх
-    for (int i = 0; i < n; i++)
-    {
-        if (abs(a[i]) == maxMagnitude)
-        {
-            indices[count] = i + 1;
-            count++;
-        }
+    // Вивід елементів масиву
+    printf("Елементи масиву: ");
+    for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
+    printf("\n");
 
-    printf("\nНомери: ");
-    for (int i = 0; i < count; i++)
-    {
-        printf("%d ", indices[i]);
+    // Вивід порядкових номерів найбільших чисел
+    printf("Номери: ");
+    for (int i = 0; i < count; i++) {
+        int index = max_indices[i] - a + 1;  // Визначення порядкового номеру
+        printf("%d ", index);
     }
+    printf("\n");
 
     return 0;
 }
